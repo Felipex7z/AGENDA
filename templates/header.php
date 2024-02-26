262a896483cb4a88f41c67edbb6c5c8502e3e6d1
Luiz Felipe Cordeiro <114431500+Felipex7z@users.noreply.github.com>
2024-02-25 22:50:56 -0300

    organizando o processamento de dados

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,14 @@
 <?php
 
   include_once("config/url.php");
- 
+  include_once("config/process.php");
+
+
+  //limpa mensagem
+  if(isset($_SESSION['msg'])){
+    $printmsg = $_SESSION['msg'];
+    $_SESSION['msg'] = '';
+  }
   
 
 
